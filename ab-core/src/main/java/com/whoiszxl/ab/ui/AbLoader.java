f73ee3afa8b224e6d5d9b576a4cf214eebc99cf6
zhouxiaolong <zhouxl@lamezhi.com>
2017-12-18 12:28:22 +0800

    集成AVLoading,并且设置loading的样式

---
+++ ab-core/src/main/java/com/whoiszxl/ab/ui/AbLoader.java
@@ -0,0 +1,17 @@
+package com.whoiszxl.ab.ui;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatDialog;
+
+/**
+ * Created by whoiszxl on 2017/12/18.
+ */
+
+public class AbLoader {
+
+    public static void showLoading(Context context,String type){
+
+        final AppCompatDialog dialog = new AppCompatDialog(context);
+    }
+
+}
---
+++ ab-core/src/main/java/com/whoiszxl/ab/ui/LoaderStyle.java
@@ -0,0 +1,37 @@
+package com.whoiszxl.ab.ui;
+
+/**
+ * Created by whoiszxl on 2017/12/18.
+ */
+@SuppressWarnings("unused")
+public enum LoaderStyle {
+    BallPulseIndicator,
+    BallGridPulseIndicator,
+    BallClipRotateIndicator,
+    BallClipRotatePulseIndicator,
+    SquareSpinIndicator,
+    BallClipRotateMultipleIndicator,
+    BallPulseRiseIndicator,
+    BallRotateIndicator,
+    CubeTransitionIndicator,
+    BallZigZagIndicator,
+    BallZigZagDeflectIndicator,
+    BallTrianglePathIndicator,
+    BallScaleIndicator,
+    LineScaleIndicator,
+    LineScalePartyIndicator,
+    BallScaleMultipleIndicator,
+    BallPulseSyncIndicator,
+    BallBeatIndicator,
+    LineScalePulseOutIndicator,
+    LineScalePulseOutRapidIndicator,
+    BallScaleRippleIndicator,
+    BallScaleRippleMultipleIndicator,
+    BallSpinFadeLoaderIndicator,
+    LineSpinFadeLoaderIndicator,
+    TriangleSkewSpinIndicator,
+    PacmanIndicator,
+    BallGridBeatIndicator,
+    SemiCircleSpinIndicator,
+    CustomIndicator
+}
