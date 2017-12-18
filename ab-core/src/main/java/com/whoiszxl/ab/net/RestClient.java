61b9a3988d295f7fbbef07330d9285a18d9d3a03
zhouxiaolong <zhouxl@lamezhi.com>
2017-12-18 14:17:44 +0800

    添加上传功能

--- ab-core/src/main/java/com/whoiszxl/ab/net/RestClient.java
+++ ab-core/src/main/java/com/whoiszxl/ab/net/RestClient.java
@@ -11,9 +11,12 @@ import com.whoiszxl.ab.net.callback.RequestCallbacks;
 import com.whoiszxl.ab.ui.AbLoader;
 import com.whoiszxl.ab.ui.LoaderStyle;
 
+import java.io.File;
 import java.util.Map;
 import java.util.WeakHashMap;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,13 +28,14 @@ import retrofit2.Callback;
 public class RestClient {
 
     private final String URL;
-    private final WeakHashMap<String,Object> PARAMS = RestCreator.getParams();
+    private final WeakHashMap<String, Object> PARAMS = RestCreator.getParams();
     private final IRequest REQUEST;
     private final ISuccess SUCCESS;
     private final IFailure FAILURE;
     private final IError ERROR;
     private final RequestBody BODY;
     private final LoaderStyle LOADER_STYLE;
+    private final File FILE;
     private final Context CONTEXT;
 
 
@@ -42,6 +46,7 @@ public class RestClient {
                       IFailure failure,
                       IError error,
                       RequestBody body,
+                      File file,
                       Context context,
                       LoaderStyle loaderStyle) {
         this.URL = url;
@@ -51,49 +56,61 @@ public class RestClient {
         this.FAILURE = failure;
         this.ERROR = error;
         this.BODY = body;
+        this.FILE = file;
         this.CONTEXT = context;
         this.LOADER_STYLE = loaderStyle;
     }
 
-    public static RestClientBuilder builder(){
+    public static RestClientBuilder builder() {
         return new RestClientBuilder();
     }
 
-    private void request(HttpMethod method){
+    private void request(HttpMethod method) {
         final RestService service = RestCreator.getRestService();
         Call<String> call = null;
 
-        if(REQUEST != null){
+        if (REQUEST != null) {
             REQUEST.onRequestStart();
         }
 
-        if(LOADER_STYLE != null){
-            AbLoader.showLoading(CONTEXT,LOADER_STYLE);
+        if (LOADER_STYLE != null) {
+            AbLoader.showLoading(CONTEXT, LOADER_STYLE);
         }
 
-        switch (method){
+        switch (method) {
             case GET:
-                call = service.get(URL,PARAMS);
+                call = service.get(URL, PARAMS);
                 break;
             case POST:
-                call = service.post(URL,PARAMS);
+                call = service.post(URL, PARAMS);
+                break;
+            case POST_RAW:
+                call = service.postRaw(URL, BODY);
                 break;
             case PUT:
-                call = service.put(URL,PARAMS);
+                call = service.put(URL, PARAMS);
+                break;
+            case PUT_RAW:
+                call = service.putRaw(URL, BODY);
                 break;
             case DELETE:
-                call = service.delete(URL,PARAMS);
+                call = service.delete(URL, PARAMS);
+                break;
+            case UPLOAD:
+                final RequestBody requestBody = RequestBody.create(MediaType.parse(MultipartBody.FORM.toString()), FILE);
+                final MultipartBody.Part body = MultipartBody.Part.createFormData("file", FILE.getName());
+                call = RestCreator.getRestService().upload(URL, body);
                 break;
             default:
                 break;
         }
 
-        if(call!=null){
+        if (call != null) {
             call.enqueue(getRequestCallback());
         }
     }
 
-    private Callback<String> getRequestCallback(){
+    private Callback<String> getRequestCallback() {
         return new RequestCallbacks(
                 REQUEST,
                 SUCCESS,
@@ -103,19 +120,34 @@ public class RestClient {
         );
     }
 
-    public final void get(){
+    public final void get() {
         request(HttpMethod.GET);
     }
 
-    public final void post(){
-        request(HttpMethod.POST);
+    public final void post() {
+        if (BODY == null) {
+            request(HttpMethod.POST);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null");
+            }
+            request(HttpMethod.POST_RAW);
+        }
+
     }
 
-    public final void put(){
-        request(HttpMethod.PUT);
+    public final void put() {
+        if (BODY == null) {
+            request(HttpMethod.PUT);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null");
+            }
+            request(HttpMethod.PUT_RAW);
+        }
     }
 
-    public final void delete(){
+    public final void delete() {
         request(HttpMethod.DELETE);
     }
 }
--- ab-core/src/main/java/com/whoiszxl/ab/net/RestClientBuilder.java
+++ ab-core/src/main/java/com/whoiszxl/ab/net/RestClientBuilder.java
@@ -8,6 +8,7 @@ import com.whoiszxl.ab.net.callback.IRequest;
 import com.whoiszxl.ab.net.callback.ISuccess;
 import com.whoiszxl.ab.ui.LoaderStyle;
 
+import java.io.File;
 import java.util.Map;
 import java.util.WeakHashMap;
 
@@ -29,64 +30,76 @@ public class RestClientBuilder {
     private RequestBody mBody = null;
     private Context mContext = null;
     private LoaderStyle mLoaderStyle = null;
+    private File mFile = null;
 
-    RestClientBuilder(){}
+    RestClientBuilder() {
+    }
 
-    public final RestClientBuilder url(String url){
+    public final RestClientBuilder url(String url) {
         this.mUrl = url;
         return this;
     }
 
-    public final RestClientBuilder params(WeakHashMap<String, Object> params){
+    public final RestClientBuilder params(WeakHashMap<String, Object> params) {
         PARAMS.putAll(params);
         return this;
     }
 
-    public final RestClientBuilder params(String key,Object value){
-        PARAMS.put(key,value);
+    public final RestClientBuilder params(String key, Object value) {
+        PARAMS.put(key, value);
         return this;
     }
 
-    public final RestClientBuilder raw(String raw){
-        this.mBody = RequestBody.create(MediaType.parse("application/json;charset=UTF-8"),raw);
+    public final RestClientBuilder file(File file) {
+        this.mFile = file;
         return this;
     }
 
-    public final RestClientBuilder success(ISuccess iSuccess){
+    public final RestClientBuilder file(String file) {
+        this.mFile = new File(file);
+        return this;
+    }
+
+    public final RestClientBuilder raw(String raw) {
+        this.mBody = RequestBody.create(MediaType.parse("application/json;charset=UTF-8"), raw);
+        return this;
+    }
+
+    public final RestClientBuilder success(ISuccess iSuccess) {
         this.mISuccess = iSuccess;
         return this;
     }
 
-    public final RestClientBuilder failure(IFailure iFailure){
+    public final RestClientBuilder failure(IFailure iFailure) {
         this.mIFailure = iFailure;
         return this;
     }
 
-    public final RestClientBuilder error(IError iError){
+    public final RestClientBuilder error(IError iError) {
         this.mIError = iError;
         return this;
     }
 
-    public final RestClientBuilder onRequest(IRequest iRequest){
+    public final RestClientBuilder onRequest(IRequest iRequest) {
         this.mIRequest = iRequest;
         return this;
     }
 
-    public final RestClientBuilder loader(Context context,LoaderStyle style){
+    public final RestClientBuilder loader(Context context, LoaderStyle style) {
         this.mContext = context;
         this.mLoaderStyle = style;
         return this;
     }
 
-    public final RestClientBuilder loader(Context context){
+    public final RestClientBuilder loader(Context context) {
         this.mContext = context;
         this.mLoaderStyle = LoaderStyle.BallSpinFadeLoaderIndicator;
         return this;
     }
 
 
-    public final RestClient build(){
-        return new RestClient(mUrl,PARAMS,mIRequest,mISuccess,mIFailure,mIError,mBody,mContext,mLoaderStyle);
+    public final RestClient build() {
+        return new RestClient(mUrl, PARAMS, mIRequest, mISuccess, mIFailure, mIError, mBody, mFile, mContext, mLoaderStyle);
     }
 
 
